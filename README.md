8a778aaaed3972ca879579ce2ca500fca924f1ea
bdaralan <bdaralan@gmail.com>
2022-01-19 13:01:42 -0800

    Initial Implementation

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # ValueRow
 
-A description of this package.
+A commonly used view for SwiftUI's `List` and `Form`. The view contains an image, a leading label, and a trailing value.
+
+## Highlight Features
+
+- Supports multiline text: Label and Value.
+- Supports individual font: Image, Label, and Value.
+- Supports tap or long press action: Image, Label, and Value.
+
+
+## Sample Code
+
+``` Swift
+import ValueRow
+
+struct CheckoutView: View {
+    
+    var body: some View {
+        Form {
+            Section("ITEM") {
+                ValueRow("Name", value: "Ice Cream")
+                    .withValue(lineLimit: 2)
+                
+                ValueRow("Price", value: "$4.00")
+                    .withValue(font: .body.monospacedDigit())
+                
+                ValueRow("Quantity", value: "3")
+                    .withValue(font: .body.monospacedDigit())
+            }
+            Section("TOTAL") {
+                ValueRow("Subtotal", symbol: "plus.circle", value: "$12.00")
+                    .withValue(font: .body.monospacedDigit())
+                
+                ValueRow("Discount", symbol: "minus.circle", value: "$3.00")
+                    .withValue(font: .body.monospacedDigit())
+                
+                ValueRow("Total", symbol: "equal.circle", value: "$9.00")
+                    .withLabel(font: .title3.weight(.bold))
+                    .withValue(font: .title3.monospacedDigit().weight(.bold))
+            }
+        }
+    }
+}
+```
--- Sources/ValueRow/ValueRow.swift
+++ Sources/ValueRow/ValueRow.swift
@@ -1,6 +1,259 @@
-public struct ValueRow {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+
+/// A value row for list which contains an image, a leading label, and a trailing value.
+///
+/// Customization methods:
+/// - ``withProperties(_:)``
+/// - ``withContent(alignment:)``
+/// - ``withImage(foreground:)``
+/// - ``withLabel(font:)``
+/// - ``withValue(action:)``
+///
+public struct ValueRow: View {
+    
+    private var components = ValueRowComponents()
+    
+    private var properties = ValueRowProperties()
+    
+    private var actions = ValueRowActions()
+        
+    public var body: some View {
+        Label(title: titleView, icon: imageView)
+    }
+    
+    private func titleView() -> some View {
+        HStack(alignment: properties.content.alignment, spacing: 0) {
+            components.label
+                .font(properties.label.font)
+                .foregroundColor(properties.label.foreground)
+                .lineLimit(properties.label.lineLimit)
+                .gesture(actions.label)
+            Spacer(minLength: properties.content.spacing)
+            components.value
+                .font(properties.value.font)
+                .foregroundColor(properties.value.foreground)
+                .lineLimit(properties.value.lineLimit)
+                .multilineTextAlignment(.trailing)
+                .gesture(actions.value)
+        }
+    }
+    
+    private func imageView() -> some View {
+        components.image
+            .foregroundColor(properties.image.foreground)
+            .font(properties.image.font)
+            .imageScale(properties.image.scale)
+            .gesture(actions.image)
+    }
+}
+
+
+// MARK: - Constructor
+
+extension ValueRow {
+    
+    public init(label: Text, image: Image? = nil, value: Text) {
+        components.image = image
+        components.label = label
+        components.value = value
+    }
+    
+    public init(_ label: LocalizedStringKey, image: Image? = nil, value: LocalizedStringKey) {
+        self.init(label: Text(label), image: image, value: Text(value))
+    }
+    
+    public init(_ label: LocalizedStringKey, image: Image? = nil, value: String) {
+        self.init(label: Text(label), image: image, value: Text(value))
+    }
+    
+    public init(_ label: LocalizedStringKey, symbol: String, value: LocalizedStringKey) {
+        self.init(label: Text(label), image: Image(systemName: symbol), value: Text(value))
+    }
+    
+    public init(_ label: LocalizedStringKey, symbol: String, value: String) {
+        self.init(label: Text(label), image: Image(systemName: symbol), value: Text(value))
+    }
+}
+
+
+// MARK: - Configuration
+
+extension ValueRow {
+    
+    // MARK: Properties
+    
+    private func withProperties<T>(_ keyPath: WritableKeyPath<ValueRowProperties, T>, _ value: T) -> ValueRow {
+        var current = self
+        current.properties[keyPath: keyPath] = value
+        return current
+    }
+    
+    public func withProperties(_ properties: ValueRowProperties) -> ValueRow {
+        withProperties(\.self, properties)
+    }
+    
+    public func withContent(alignment: VerticalAlignment) -> ValueRow {
+        withProperties(\.content.alignment, alignment)
+    }
+    
+    public func withContent(spacing: CGFloat?) -> ValueRow {
+        withProperties(\.content.spacing, spacing)
+    }
+    
+    public func withImage(font: Font?) -> ValueRow {
+        withProperties(\.image.font, font)
+    }
+    
+    public func withImage(scale: Image.Scale) -> ValueRow {
+        withProperties(\.image.scale, scale)
+    }
+    
+    public func withImage(foreground: Color) -> ValueRow {
+        withProperties(\.image.foreground, foreground)
+    }
+    
+    public func withLabel(font: Font?) -> ValueRow {
+        withProperties(\.label.font, font)
+    }
+    
+    public func withLabel(foreground: Color) -> ValueRow {
+        withProperties(\.label.foreground, foreground)
+    }
+    
+    public func withLabel(lineLimit: Int?) -> ValueRow {
+        withProperties(\.label.lineLimit, lineLimit)
+    }
+    
+    public func withValue(font: Font?) -> ValueRow {
+        withProperties(\.value.font, font)
+    }
+    
+    public func withValue(foreground: Color) -> ValueRow {
+        withProperties(\.value.foreground, foreground)
+    }
+    
+    public func withValue(lineLimit: Int?) -> ValueRow {
+        withProperties(\.value.lineLimit, lineLimit)
+    }
+    
+    // MARK: Actions
+    
+    private func withActions<T>(_ keyPath: WritableKeyPath<ValueRowActions, T>, _ value: T) -> ValueRow {
+        var current = self
+        current.actions[keyPath: keyPath] = value
+        return current
+    }
+    
+    public func withImage(action: ValueRowActions.ActionType?) -> ValueRow {
+        withActions(\.image, action)
+    }
+    
+    public func withLabel(action: ValueRowActions.ActionType?) -> ValueRow {
+        withActions(\.label, action)
+    }
+    
+    public func withValue(action: ValueRowActions.ActionType?) -> ValueRow {
+        withActions(\.value, action)
+    }
+}
+
+
+// MARK: - ValueRowComponents
+
+private struct ValueRowComponents {
+    var image: Image?
+    var label: Text?
+    var value: Text?
+}
+
+
+// MARK: - ValueRowProperties
+
+public struct ValueRowProperties {
+    
+    public var content = Content()
+    public var image = Image()
+    public var label = Label()
+    public var value = Value()
+    
+    public init() {}
+    
+    public struct Content {
+        public var alignment: VerticalAlignment = .center
+        public var spacing: CGFloat?
+    }
+
+    public struct Image {
+        public var font: Font?
+        public var scale: SwiftUI.Image.Scale = .medium
+        public var foreground: Color = .accentColor
+    }
+
+    public struct Label {
+        public var font: Font?
+        public var foreground: Color = .primary
+        public var lineLimit: Int?
+    }
+
+    public struct Value {
+        public var font: Font?
+        public var foreground: Color = .secondary
+        public var lineLimit: Int?
+    }
+}
+
+
+// MARK: - ValueRowActions
+
+public struct ValueRowActions {
+    
+    var image: ActionType?
+    var label: ActionType?
+    var value: ActionType?
+    
+    public enum ActionType {
+        
+        /// A tap action.
+        ///
+        /// - Parameters:
+        ///   - count: The required number of tap events.
+        ///   - action: The action to perform.
+        case tap(count: Int = 1, action: () -> Void)
+        
+        /// A long press action.
+        ///
+        /// - Parameters:
+        ///   - minDuration: The minimum duration of the long press that must elapse before the gesture succeeds.
+        ///   - maxDistance: The maximum distance that the long press can move before the gesture fails.
+        ///   - action: The action to perform.
+        case press(minDuration: Double = 0.5, maxDistance: Double = 10, action: (LongPressGesture.Value) -> Void)
+        
+        /// A long press action.
+        ///
+        /// - Parameters:
+        ///   - minDuration: The minimum duration of the long press that must elapse before the gesture succeeds.
+        ///   - maxDistance: The maximum distance that the long press can move before the gesture fails.
+        ///   - action: The action to perform.
+        public static func press(minDuration: Double = 0.5, maxDistance: Double = 10, action: @escaping () -> Void) -> ActionType {
+            .press(minDuration: minDuration, maxDistance: maxDistance, action: { _ in action() })
+        }
+    }
+}
+
+
+// MARK: - View Helper
+
+private extension View {
+    
+    @ViewBuilder func gesture(_ type: ValueRowActions.ActionType?) -> some View {
+        switch type {
+        case nil:
+            self
+        case let .tap(count, action):
+            self.gesture(TapGesture(count: count).onEnded(action))
+        case let .press(duration, distance, action):
+            self.gesture(LongPressGesture(minimumDuration: duration, maximumDistance: distance).onEnded(action))
+        }
     }
 }
